c37eafafe4114c91f2eaf95d5e5d3b6892d1b958
Amanda Alves <amandagsal@gmail.com>
2023-05-04 18:17:54 -0300

    Implementa controller para customer

---
+++ src/main/java/com/alves/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
@@ -0,0 +1,15 @@
+package com.alves.hexagonal.adapters.in.controller.mapper;
+
+import com.alves.hexagonal.adapters.in.controller.request.CustomerRequest;
+import com.alves.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "address", ignore = true)
+    @Mapping(target = "isValidCpf", ignore = true)
+    Customer toCustomer(CustomerRequest customerRequest);
+}
