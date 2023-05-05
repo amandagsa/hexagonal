7f3fa7a4f2519ea5708a66807466def70ab3c18d
Amanda Alves <amandagsal@gmail.com>
2023-05-05 14:52:51 -0300

    Implementa findById para customer

---
+++ src/main/java/com/alves/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -0,0 +1,18 @@
+package com.alves.hexagonal.application.core.usecase;
+
+import com.alves.hexagonal.application.core.domain.Customer;
+import com.alves.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id) {
+        return findCustomerByIdOutputPort.find(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+}
